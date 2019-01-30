fedb5b997951802a64c52b7a6c77f59827ea71df
Prasanth <prasanthlouis21@gmail.com-at-383762989543>
2019-01-29 22:14:39 -0500

    Intial commit

---
+++ Backend/RSVPBackend/RSVPBackend/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace RSVPBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileStream = new FileStream("Emails.txt", FileMode.Open);
+            var filesContents = string.Empty;
+            using (var streamReader = new StreamReader(fileStream))
+            {
+                filesContents = streamReader.ReadToEnd();
+            }
+
+            var emails = filesContents.Split(',').ToList();
+            var smtp = new SMTPClient();
+            foreach(var email in emails)
+            {
+                smtp.SendEmail(email);
+            }
+            
+
+        }
+    }
+}
---
+++ Backend/RSVPBackend/RSVPBackend/SMTPClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace RSVPBackend
+{
+    public class SMTPClient
+    {
+        public void SendEmail(string email)
+        {
+            using (var message = new MailMessage())
+            {
+                message.To.Add(new MailAddress("prasanthlouis21@gmail.com", "Prasanth Louis"));
+                message.From = new MailAddress("prasanthlouis21@gmail.com", "PMoney$$");
+                message.CC.Add(new MailAddress("prasanthlouis21@mailinator.com", "Burner Account"));
+                message.Subject = "Are you coming for super bowl weekend";
+                message.Body = "Please reply yes or no";
+                message.IsBodyHtml = true;        
+                var fileStream = new FileStream("Password.txt", FileMode.Open);
+                var password = string.Empty;
+                using (var streamReader = new StreamReader(fileStream))
+                {
+                    password = streamReader.ReadToEnd();
+                }
+
+                using (var client = new SmtpClient("smtp.gmail.com"))
+                {
+                    client.Port = 587;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.Credentials = new NetworkCredential("TwitchBurner21", password);
+                    client.EnableSsl = true;
+                    client.Send(message);
+                }
+            }
+        }
+    }
+}
